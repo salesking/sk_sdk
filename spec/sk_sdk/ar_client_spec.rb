0eb6ca05bde460fe1335ebc62bf9501f91793f11
georg <georg@minimi>
2011-03-14 00:34:33 +0100

    added spec with live connection

--- spec/sk_sdk/ar_client_spec.rb
+++ spec/sk_sdk/ar_client_spec.rb
@@ -7,14 +7,12 @@ describe SK::SDK::ArClient, "make new class" do
     Client.set_connection( CONNECTION )
   end
 
-  it "should create class from schema" do
-    # hash from json schema
+  it "should create class" do
     c = Client.new
     c.first_name = 'herbert' # implicit setter
     c.first_name.should == 'herbert' # implicit getter
     c1 = Client.new
   end
-
 
   it "should have properties as attributes" do
     c = Client.new :some_field => ''
@@ -22,7 +20,7 @@ describe SK::SDK::ArClient, "make new class" do
   end
 
   it "should create save method" do
-    c = Client.new
+    c = Client.new 
     c.respond_to?(:save).should be_true
   end
 
@@ -43,5 +41,21 @@ describe SK::SDK::ArClient, "make new class" do
       c = SK::API::Client.new
       c.id
     }.should_not raise_error(RuntimeError)
+  end
+end
+
+describe SK::SDK::ArClient, "with real connection" do
+
+  before :all do
+    SK::SDK::ArClient.make(:client) unless Object.const_defined?('Client')
+    Client.set_connection( CONNECTION )
+    #TODO check if sk is available
+  end
+
+  it "should save" do
+    c = Client.new :organisation=>"Rack'n Roll"
+    c.save.should be_true
+    c.id.should_not be_empty
+    c.number.should_not be_empty
   end
 end
