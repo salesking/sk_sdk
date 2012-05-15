7312508c6c3b522043c6106ab40595a0396b176e
Mike Połtyn <mike@railslove.com>
2012-05-15 15:38:36 +0200

    give back proper error messages

--- lib/sk_sdk/ar_patches/ar3/validations.rb
+++ lib/sk_sdk/ar_patches/ar3/validations.rb
@@ -5,8 +5,12 @@ module ActiveResource
     # message, which we dont supply => only field name is used in returned error msg
     def from_array(messages, save_cache=false)
       clear unless save_cache
-      messages.each do |msg|
-        add msg[0], msg[1]
+      messages.each do |message_hash|
+        message_hash.each do |attr_name, attr_values|
+          attr_values.each do |attr_value|
+            add attr_name, attr_value
+          end
+        end
       end
     end
   end #Errors
