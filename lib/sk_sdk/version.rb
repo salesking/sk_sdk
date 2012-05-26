16d55d986979c9681793268f38ad5bfc95ccf575
Georg Leciejewski <gl@salesking.eu>
2012-05-26 14:12:49 +0200

    remove jeweler

---
+++ lib/sk_sdk/version.rb
@@ -0,0 +1,5 @@
+module SK
+  module SDK
+    VERSION = '0.1.1'
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,21 @@
+# encoding: utf-8
+$:.unshift(File.dirname(__FILE__))
+$:.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+
+require 'simplecov'
+SimpleCov.start 'rails'
+SimpleCov.coverage_dir 'coverage'
+
 require 'rubygems'
 require 'yaml'
 require 'rspec'
-require "active_support"
-require "active_support/json"
-require "#{File.dirname(__FILE__)}/../lib/sk_sdk"
-require "#{File.dirname(__FILE__)}/../lib/sk_sdk/base"
-require "#{File.dirname(__FILE__)}/../lib/sk_sdk/sync"
-require "#{File.dirname(__FILE__)}/../lib/sk_sdk/oauth"
-require "#{File.dirname(__FILE__)}/../lib/sk_sdk/signed_request"
+require 'active_support'
+require 'active_support/json'
+require 'sk_sdk'
+require 'sk_sdk/base'
+require 'sk_sdk/sync'
+require 'sk_sdk/oauth'
+require 'sk_sdk/signed_request'
 
 
 puts "Testing with ActiveResource v: #{ActiveResource::VERSION::STRING}"
@@ -22,9 +30,9 @@ def oauth_settings
 end
 
 def get_settings
-  @settings ||= begin 
-    YAML.load_file(File.join(File.dirname(__FILE__), 'settings.yml')) 
+  @settings ||= begin
+    YAML.load_file(File.join(File.dirname(__FILE__), 'settings.yml'))
     rescue => e
-    puts "Missing settings.yml in rails_root/spec/settings.yml"
+    puts 'Missing settings.yml in rails_root/spec/settings.yml'
   end
 end
